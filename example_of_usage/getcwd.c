1b8cc12de2ce2527f47290e04ca9f273ceef931c
lyfhael <singinganthlers@hotmail.com>
2023-03-28 16:30:51 +0200

    [ADD] getcwd usage example

---
+++ example_of_usage/getcwd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getcwd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 16:15:30 by hateisse          #+#    #+#             */
+/*   Updated: 2023/03/28 16:30:15 by hateisse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*current_dir;
+
+	// On appel getcwd pour obtenir le repertoire dans lequel on est.
+	// On lui donne NULL et 0 en parametre pour qu'il puisse malloc la taille necessaire
+	current_dir = getcwd(NULL, 0);
+	// Si le retour est NULL il y a eu une erreur.
+	if (!current_dir)
+		perror("getcwd");
+	else
+		printf("%s\n", current_dir);
+	return (0);
+}
