d566410ec90fd0cc8095115d1feb8c68a1346e33
Malfwat <adam_972@hotmail.fr>
2023-03-29 15:28:25 +0200

    [ADD]: access read open close

--- example_of_usage/read_line.c
+++ example_of_usage/read_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_line.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 14:13:04 by malfwa            #+#    #+#             */
-/*   Updated: 2023/03/28 16:13:23 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/03/29 14:39:40 by malfwa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 
 int	main(void)
 {
-	printf("%s\n", readline("This is my prompt: "));
+	readline("This is my prompt: ")
+	printf("On a un prompt: %s et une ligne obtenue %s \n", rl_prompt, rl_line_buffer);
+	free(rl_line_buffer);
+	free(rl_prompt);
 	return (0);
 }
+
+/*
+Here readline(char *prompt) will display a prompt and wait for the user to write something
+when it's done the commande line will be stored in the variable rl_line_buffer and prompt will be stored in rl_prompt
+Both need to be freed
+*/
