441f93fc05c72bfce7f02d0c738c6b7e26e3620a
lyfhael <singinganthlers@hotmail.com>
2023-03-29 20:50:46 +0200

    [ADD] ttyslot.c usage example

--- example_of_usage/ttyslot.c
+++ example_of_usage/ttyslot.c
@@ -6,21 +6,25 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 17:01:16 by hateisse          #+#    #+#             */
-/*   Updated: 2023/03/29 17:01:22 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/03/29 20:43:27 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-// Renvoie 
+int main(void) {
+    // Get the number of the current process's terminal device
+    int tty_number = ttyslot();
 
-int	main(void)
-{
-	int	slot;
+	sleep(5);
+    if (tty_number == 0) {
+        printf("Error: no controlling terminal attached.\n");
+        return 1;
+    }
 
-	slot = ttyslot();
-	printf("%d\n", slot);
-	return (0);
+    // Print the number of the terminal device
+    printf("Terminal device number: %d\n", tty_number);
+
+    return 0;
 }
