de4047d928937de1e917e246df83f5b05f495f6c
Malfwat <adam_972@hotmail.fr>
2023-04-04 11:36:52 +0200

    [ADD]: echo variadique function

---
+++ srcs/built_in/echo.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 11:20:41 by malfwa            #+#    #+#             */
+/*   Updated: 2023/04/04 11:31:58 by malfwa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+#include <stdarg.h>
+#include <libft.h>
+#include <unistd.h>
+
+void	echo(bool nl, int nb, ...)
+{
+	va_list	ap;
+
+	va_start(ap, nb);
+	while (nb--)
+	{
+		ft_putstr_fd(va_arg(ap, char *), 1);
+		if (nb)
+			write(1, " ", 1);
+	}
+	if (nl)
+		write(1, "\n", 1);
+}
