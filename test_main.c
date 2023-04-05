7b0fe3397598126fa1e364aa221ba7d7ead1546a
hateisse <hateisse@student.42.fr>
2023-04-05 19:36:59 +0200

    [ADD]: personalized prompt

--- test_main.c
+++ test_main.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   test_main.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 14:25:00 by malfwa            #+#    #+#             */
-/*   Updated: 2023/04/05 14:27:59 by malfwa           ###   ########.fr       */
+/*   Updated: 2023/04/05 15:49:19 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+#include <libft.h>
 #include <struct_ms.h>
-#include <parsing.h>
+#include <parsing_ms.h>
 
 int	main(void)
 {
 	t_block	*head;
 
 	head = NULL;
-	return (parse_cmd(&head, "ecse sef sef && sfs | sef"));
+	return (parse_cmd(&head, ft_strdup("ok ls && ls")));
 }
