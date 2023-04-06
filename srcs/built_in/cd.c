99a29247069854a145ce410998772fb3e9e34a5c
Malfwat <adam_972@hotmail.fr>
2023-04-06 05:22:29 +0200

    [ADD]: cd update

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 01:33:59 by malfwa            #+#    #+#             */
-/*   Updated: 2023/04/06 04:52:16 by malfwa           ###   ########.fr       */
+/*   Updated: 2023/04/06 05:21:40 by malfwa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	cd(t_env_var	*head, char *str)
 		perror("cd");
 		return (1);
 	}
-	export(&head, ft_strdup("PWD="), getcwd(NULL, 0));
+	export(&head, ft_strdup("PWD="), getcwd(NULL, 0), 0);
 	return (0);
 }
