84de1d8992ff13ffdda3c8658861d8f40e1bf5ab
Malfwat <adam_972@hotmail.fr>
2023-04-06 05:21:14 +0200

    [ADD]: update env function

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -6,14 +6,14 @@
 /*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 01:33:59 by malfwa            #+#    #+#             */
-/*   Updated: 2023/04/04 01:53:59 by malfwa           ###   ########.fr       */
+/*   Updated: 2023/04/06 04:52:16 by malfwa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <env_function.h>
 #include <libft.h>
-#include <struct.h>
+#include <struct_ms.h>
 
 int	cd(t_env_var	*head, char *str)
 {
@@ -22,6 +22,6 @@ int	cd(t_env_var	*head, char *str)
 		perror("cd");
 		return (1);
 	}
-	export(&head, ft_strjoin(ft_strdup("PWD="), getcwd(NULL, 0)));
+	export(&head, ft_strdup("PWD="), getcwd(NULL, 0));
 	return (0);
 }
