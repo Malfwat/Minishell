dd0bce59a839542c7995454d1d2ffdcb29120b89
Malfwat <adam_972@hotmail.fr>
2023-04-07 03:34:59 +0200

    [ADD]: history

---
+++ srcs/history/history_functions.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   history_functions.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 02:18:56 by malfwa            #+#    #+#             */
+/*   Updated: 2023/04/07 03:33:29 by malfwa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <get_next_line.h>
+#include <readline/history.h>
+#include <fcntl.h>
+
+int	get_my_history(void)
+{
+	char	*str;
+	int		fd;
+	char	*tmp;
+
+	tmp = ft_strjoin(getenv("HOME"), "/.my_history");
+	fd = open(tmp, O_RDWR | O_APPEND | O_CREAT, 0644);
+	free(tmp);
+	if (fd == -1)
+		return (-1);
+	get_next_line(fd, &str);
+	while (str)
+	{
+		if (str[ft_strlen(str) - 1] == '\n')
+			str[ft_strlen(str) - 1] = 0;
+		add_history(str);
+		free(str);
+		get_next_line(fd, &str);
+	}
+	return (fd);
+}
+
+void	my_add_history(char *str, int fd)
+{
+	if (!*str)
+		return ;
+	write(fd, str, ft_strlen(str));
+	write(fd, "\n", 1);
+	add_history(str);
+}
--- srcs/test.c
+++ srcs/test.c
@@ -6,12 +6,20 @@
 /*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:12:21 by hateisse          #+#    #+#             */
-/*   Updated: 2023/04/07 01:48:35 by malfwa           ###   ########.fr       */
+/*   Updated: 2023/04/07 03:31:53 by malfwa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <prompt.h>
+
+// #include <libft.h>
+// #include <get_next_line.h>
+// #include <readline/history.h>
+// #include <fcntl.h>
+
+#include <history.h>
+
 
 
 int	main(void)
@@ -27,8 +35,13 @@ int	main(void)
 	type = -1;
 	if (!isatty(0) || !isatty(1) || !isatty(2))
 		return (perror("minishell"), 1);
+	int fd = get_my_history();
+	if (fd == -1)
+		return (2);
 	readline(build_prompt(prompt_params));
+	my_add_history(rl_line_buffer, fd);
 	parse_cmd(&head, rl_line_buffer);
 	free(rl_prompt);
+	close(fd);
 	printf("[%d] res: '%s'\n", type, res);
 }
