e0543a4db1652224ce0ea9b68d86968c83d1df8a
hateisse <hateisse@student.42.fr>
2023-04-08 21:41:58 +0200

    [MAJ/FIX]: deplacement de exit_minishell dans son fichier. et differents fix pas important

--- srcs/struct_utils/free_structs_1.c
+++ srcs/struct_utils/free_structs_1.c
@@ -6,7 +6,7 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 04:25:21 by marvin            #+#    #+#             */
-/*   Updated: 2023/04/08 19:15:28 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/04/08 21:34:16 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 #include <minishell.h>
 #include <stdlib.h>
 
-void	free_ms_params(t_minishell *ms_params)
+void	free_ms_params(t_minishell ms_params)
 {
-	free_env_lst(ms_params->envp);
-	ms_params->envp = NULL;
-	free(ms_params);
+	free_env_lst(ms_params.envp);
+	ms_params.envp = NULL;
 }
 
 void	free_env_lst(t_env_var *envp_lst)
