7141674887e46591a6d7c834cea6dcc57372b9e7
adam <amouflet@e2r7p21.clusters.42paris.fr>
2023-04-13 15:21:42 +0200

    [MAJ]: edit parsing function to take t_split_arg argument

---
+++ .build/parsing/input_output.d
@@ -0,0 +1,13 @@
+.build/parsing/input_output.o: srcs/parsing/input_output.c \
+  includes/parsing_ms.h includes/struct_ms.h includes/ms_define.h \
+  libft/libft.h libft/get_next_line/get_next_line.h
+
+includes/parsing_ms.h:
+
+includes/struct_ms.h:
+
+includes/ms_define.h:
+
+libft/libft.h:
+
+libft/get_next_line/get_next_line.h:
