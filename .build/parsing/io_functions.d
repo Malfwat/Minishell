fed23b959d5369f0f938394b8ef4e923e17d6395
adam <amouflet@e2r7p21.clusters.42paris.fr>
2023-04-13 15:21:42 +0200

    [MAJ]: edit parsing function to take t_split_arg argument

---
+++ .build/parsing/io_functions.d
@@ -0,0 +1,13 @@
+.build/parsing/io_functions.o: srcs/parsing/io_functions.c \
+  includes/parsing_ms.h includes/struct_ms.h libft/libft.h \
+  libft/get_next_line/get_next_line.h includes/ms_define.h
+
+includes/parsing_ms.h:
+
+includes/struct_ms.h:
+
+libft/libft.h:
+
+libft/get_next_line/get_next_line.h:
+
+includes/ms_define.h:
