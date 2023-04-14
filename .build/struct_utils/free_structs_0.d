d78895360cb8510cd2b01d0e9375d6f842581e3b
teisseire117 <hateisse@student.42.fr>
2023-04-14 22:20:45 +0200

    [ADD]:  gere, io split args gere, ca compile

--- .build/struct_utils/free_structs_0.d
+++ .build/struct_utils/free_structs_0.d
@@ -1,4 +1,6 @@
 .build/struct_utils/free_structs_0.o: srcs/struct_utils/free_structs_0.c \
-  includes/struct_ms.h
+  includes/struct_ms.h includes/minishell.h
 
 includes/struct_ms.h:
+
+includes/minishell.h:
