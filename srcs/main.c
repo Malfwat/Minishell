c35753bd24c70fc1d64e6b7e9b95910e1fae3b99
teisseire117 <hateisse@student.42.fr>
2023-04-15 06:56:00 +0200

    [FIX] typos in re-organisation of init... funcs in main

--- srcs/main.c
+++ srcs/main.c
@@ -505,7 +505,7 @@ void	init_prompt(t_minishell *ms_params, char **user_input)
 	char		*ms_prompt;
 
 	if (!refresh_prompt_param(&ms_params->prompt_params))
-		exit_ms(ms_params, 0, "prompt");
+		exit_ms(*ms_params, 0, "prompt");
 	ensure_prompt_position();
 	ms_prompt = build_prompt(&ms_params->prompt_params);
 	if (!ms_prompt)
