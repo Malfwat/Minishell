669e5b8fa25d79968121f66345c430f6dc12bcb6
teisseire117 <hateisse@student.42.fr>
2023-04-16 20:23:38 +0200

    [FIX] parsing: gestion cas type 'echo test &&' ou l'operand de droite est vide

--- .build/parsing/parsing.d
+++ .build/parsing/parsing.d
@@ -1,7 +1,7 @@
 .build/parsing/parsing.o: srcs/parsing/parsing.c includes/struct_ms.h \
- includes/ms_define.h includes/parsing_ms.h includes/minishell.h \
- includes/env_function.h libft/libft.h \
- libft/get_next_line/get_next_line.h
+  includes/ms_define.h includes/parsing_ms.h includes/minishell.h \
+  includes/env_function.h libft/libft.h \
+  libft/get_next_line/get_next_line.h
 
 includes/struct_ms.h:
 
--- .build/struct_utils/free_structs_0.d
+++ .build/struct_utils/free_structs_0.d
@@ -1,6 +1,11 @@
 .build/struct_utils/free_structs_0.o: srcs/struct_utils/free_structs_0.c \
- includes/struct_ms.h includes/minishell.h
+  includes/struct_ms.h includes/minishell.h libft/libft.h \
+  libft/get_next_line/get_next_line.h
 
 includes/struct_ms.h:
 
 includes/minishell.h:
+
+libft/libft.h:
+
+libft/get_next_line/get_next_line.h:
