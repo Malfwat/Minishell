ecf6fe75680ddd1800221642575d2e97e81c14c3
= <vyln10@hotmail.com>
2023-04-16 08:29:02 +0200

    [FIX] try new way to reset prompt on CTRL+C

--- srcs/main.c
+++ srcs/main.c
@@ -539,11 +539,10 @@ bool	parse_user_input(t_minishell *ms_params, char *user_input)
 void	handler_func(int num)
 {
 	(void)num;
-	dprintf(0, "\xFF");
-	// printf("\n");
-	// rl_replace_line("", 0); // Clear the previous text
-	// rl_on_new_line();      // move the cursor to a new line
-	// rl_redisplay();
+	printf("\n");
+	rl_replace_line("", 0); // Clear the previous text
+	rl_on_new_line();      // move the cursor to a new line
+	rl_redisplay();
 	// rl_forced_update_display();
 }
 
