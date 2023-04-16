d64e016512871f68f4163ed5d967b5370cee47c0
= <vyln10@hotmail.com>
2023-04-16 08:49:08 +0200

    [ADD] Gestion CTRL + D (quitte minishell)

--- srcs/main.c
+++ srcs/main.c
@@ -539,6 +539,7 @@ bool	parse_user_input(t_minishell *ms_params, char *user_input)
 void	handler_func(int num)
 {
 	(void)num;
+	write(1, "\n", 1);
 	rl_replace_line("", 0); // Clear the previous text
 	rl_on_new_line();	// move the cursor to a new line
 	rl_redisplay();
