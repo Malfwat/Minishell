31654b62249960187f2443cfaf685513ae149b87
Malfwat <adam_972@hotmail.fr>
2023-04-18 23:53:09 +0200

    [MAJ]: a la norme

--- includes/history.h
+++ includes/history.h
@@ -6,12 +6,14 @@
 /*   By: malfwa <malfwa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 03:14:25 by malfwa            #+#    #+#             */
-/*   Updated: 2023/04/18 23:38:16 by malfwa           ###   ########.fr       */
+/*   Updated: 2023/04/18 23:52:59 by malfwa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HISTORY_H
 # define HISTORY_H
+
+# include <struct_ms.h>
 
 int		get_my_history(t_minishell *ms_params);
 void	ms_add_history(char *str, t_minishell *ms_params);
