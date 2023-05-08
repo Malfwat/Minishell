ef8cf2fb7378adfd928cfe0bb0e327d03157c883
teisseire117 <hateisse@student.42.fr>
2023-05-08 06:32:35 +0200

    debut implementation minishell banner

--- srcs/shell_banner/ms_banner.sh
+++ srcs/shell_banner/ms_banner.sh
@@ -28,17 +28,17 @@ while true; do
 	random_chance=$((RANDOM % 100 + 1))
 
 	for i in `eval $e {1..$R}`; do
-		c=`printf '%c' $(shuf -n1 -e m i n i s h e l l M I N I S H E L L ═ ║ ╔ ╗ ╚ ╝ █ ░)`
+		c=`printf "%s" $(shuf -n1 -e m i n i s h e l l M I N I S H E L L ═ ║ ╔ ╗ ╚ ╝ █ ░)`
 		p_char="\033[$[i-1];${j}H\033[32m$c"
 		curr_char="\033[$i;${j}H\033[37m$c"
-		char_in_banner=$(awk -v row=$(($R - $i + 1)) -v col=$j 'BEGIN{FS=""; OFS="";} NR==row {printf "%s", $col; exit}' <<< "$REVERSED_BANNER")
+		char_in_banner=$(perl -C -ne 'BEGIN{$row='$(($R - $i + 1))'; $col='$j'} if($. == $row) {print substr($_, $col, 1); exit}' <<< "$REVERSED_BANNER")
 
 		if [[ -n "$char_in_banner" && "$char_in_banner" != " " ]] && [ $random_chance -le 100 ]; then
 			p_char="\033[$[i-1];${j}H\033[32m$char_in_banner"
 			curr_char="\033[$i;${j}H\033[37m$char_in_banner"
 			$e $p_char$curr_char
 		else
-			char_in_banner=$(tac "minishell_banner.txt" | awk -v row=$(($R - $i + 1 - 1)) -v col=$j 'BEGIN{FS=""; OFS="";} NR==row {printf "%s", $col; exit}')
+			char_in_banner=$(tac "minishell_banner.txt" | perl -C -ne 'BEGIN{$row='$(($R - $i))'; $col='$j'} if($. == $row) {print substr($_, $col, 1); exit}')
 			if [[ -n "$char_in_banner" && "$char_in_banner" != " " ]] && [ $random_chance -le 100 ]; then
 				$e $curr_char
 			else
