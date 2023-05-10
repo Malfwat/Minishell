8231384fefe3a0e415185d8ae9f1b3e19558d31c
lyfhael <singinganthlers@hotmail.com>
2023-05-10 15:38:33 +0200

    [FIX]: dollar var stop char (ex: @, first @ is a stopchar, so it's  and @ is printed additionally)

--- srcs/execution/meta_char.c
+++ srcs/execution/meta_char.c
@@ -6,7 +6,7 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 05:54:10 by malfwa            #+#    #+#             */
-/*   Updated: 2023/05/09 17:57:05 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:33:50 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,10 @@ char	*replace_dollars_var(char *res, t_env *envp, char *var)
 	char	*dollar_var;
 	char	*tmp;
 
-	tmp = ft_strchrnul_set(var, "\'* ,!@#%^&()-+=[]{};:\"<>\t.?/\\|");
+	if (*var && ft_strchr("0123456789@!?#*", *var))
+		tmp = var + 1;
+	else
+		tmp = ft_strchrnul_set(var, "\'* @,#%^&()-+=[]{};:\"<>\t.?!/\\|");
 	dollar_var = ft_substr(var, 0, tmp - var);
 	env_var = find_env_var(envp, dollar_var);
 	if (!env_var)
