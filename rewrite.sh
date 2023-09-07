e717b4d0f85e183c57e33e47641029f6e98ca964
teisseire117 <hateisse@student.42.fr>
2023-09-07 16:01:24 +0200

    remove rewrite.sh

--- rewrite.sh
+++
@@ -1,9 +0,0 @@
-git filter-branch --env-filter -f '
-if [ "$GIT_AUTHOR_EMAIL" = "oldemailhere" ] && [ "$GIT_AUTHOR_NAME" != "oldusernamehere" ];
-then
-    GIT_AUTHOR_EMAIL="newemailhere";
-    GIT_AUTHOR_NAME="usernamehere";
-fi
-export GIT_AUTHOR_EMAIL;
-export GIT_AUTHOR_NAME;
-' --tag-name-filter cat -- --branches --tags
